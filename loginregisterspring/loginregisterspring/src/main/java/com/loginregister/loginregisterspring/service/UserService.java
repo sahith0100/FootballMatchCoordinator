cf48cf5adf71761fe1ac252d9f9995a914ee8019
sahith <sahith0100@gmail.com>
2022-07-03 13:07:39 -0700

    loginpage

--- loginregisterspring/loginregisterspring/src/main/java/com/loginregister/loginregisterspring/service/UserService.java
+++ loginregisterspring/loginregisterspring/src/main/java/com/loginregister/loginregisterspring/service/UserService.java
@@ -2,9 +2,12 @@ package com.loginregister.loginregisterspring.service;
 
 import com.loginregister.loginregisterspring.model.User;
 import com.loginregister.loginregisterspring.web.dto.UserRegistrationDto;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 
 public interface UserService {
     User save(UserRegistrationDto registrationDto);
 
+    UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
 }
